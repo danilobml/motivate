be33cc70c021b853d7a7b70b094d558da255854d
danilobml <danbarml@gmail.com>
2025-10-19 16:16:38 +0200

    feat: logger and recover middlewares

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -2,11 +2,14 @@ package handlers
 
 import (
 	"net/http"
+
+	"github.com/danilobml/motivate/internal/httpx/middleware"
 )
 
-func RegisterRoutes() *http.ServeMux {
+func RegisterRoutes() http.Handler {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("GET /health", getHealthHandler)
 
-	return mux
+	return middleware.Logger(middleware.Recover(mux))
 }
